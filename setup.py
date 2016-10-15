08b45de072533c1c028358578a31a24c8964ebfe
Ryosuke Okuta <okuta@preferred.jp>
2016-10-15 19:53:10 +0900

    Simplify build process on Read the Docs

--- setup.py
+++ setup.py
@@ -16,14 +16,6 @@ install_requires = [
     'six>=1.9.0',
 ]
 
-
-# Hack for Read the Docs
-on_rtd = chainer_setup_build.check_readthedocs_environment()
-if on_rtd:
-    print('Add develop command for Read the Docs')
-    sys.argv.insert(1, 'develop')
-    setup_requires = ['Cython>=0.23'] + setup_requires
-
 chainer_setup_build.parse_args()
 
 setup(
